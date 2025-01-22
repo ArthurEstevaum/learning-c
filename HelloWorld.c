f7d6161f835ec10c5b6ece2eaa506dad0a3b8799
ArthurEstevaum <arthurestevao631@gmail.com>
2025-01-21 21:07:57 -0300

    Initial commit

---
+++ HelloWorld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x, y;
+    char name[25];
+
+    printf("CALCULADORA DE POTÊNCIAS\n");
+    printf("Digite o valor da base: ");
+    scanf("%d", &x);
+    printf("Digite o valor do expoente: ");
+    scanf("%d", &y);
+    getchar();
+    printf("A potência com a base %d e expoente %d é igual a: %0.lf\n", x, y, pow(x,y));
+    printf("Qual seu nome?");
+    fgets(name, sizeof(name), stdin);
+    printf("\nOlá, %s", name);
+}
